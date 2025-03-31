e8a04011b8405e5ca4a86ab3f1e8dfede7fdc583
Sahithinampally <sahithi.nampally@gmail.com>
2025-03-31 22:40:09 +0530

    updated settings

--- wsgi.py
+++ wsgi.py
@@ -8,10 +8,9 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'advantage.settings')  # Replace with your actual project name
 
 application = get_wsgi_application()
-app = get_wsgi_application() 
+app = application  # This is needed for Vercel

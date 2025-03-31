0938e473519545b6277c9480b9a6592bcace1207
Sahithinampally <sahithi.nampally@gmail.com>
2025-03-31 22:27:59 +0530

    updated settings

--- settings.py
+++ settings.py
@@ -41,7 +41,7 @@ DEBUG = True
 
 load_dotenv()  # Load environment variables
 
-ALLOWED_HOSTS = ['advantage-eight.vercel.app','advantage-git-main-nsahithi04s-projects.vercel.app','advantage-f8sw0cqzb-nsahithi04s-projects.vercel.app','127.0.0.1']
+ALLOWED_HOSTS = ['advantage-eight.vercel.app','advantage-git-main-nsahithi04s-projects.vercel.app','advantage-f8sw0cqzb-nsahithi04s-projects.vercel.app','127.0.0.1','advantage-1nuq1k9b9-nsahithi04s-projects.vercel.app']
 
 
 # Application definition

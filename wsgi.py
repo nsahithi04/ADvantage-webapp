d8430c1425d8a613e21d180c78d98b60c2fd2a81
Sahithinampally <sahithi.nampally@gmail.com>
2025-04-01 11:38:32 +0530

    updated manage

--- wsgi.py
+++ wsgi.py
@@ -8,9 +8,14 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
 import os
+import sys
+
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')  # Replace with your actual project name
+# Add the project directory to the Python path
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-app  = get_wsgi_application()
-application = get_wsgi_application() 
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
+
+application = get_wsgi_application()
+app=get_wsgi_application()
